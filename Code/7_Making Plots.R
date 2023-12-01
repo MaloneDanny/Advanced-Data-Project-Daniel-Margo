433bf10378a1ab9c24ccb2431ad006cf635abb75
margolaclair <143371207+margolaclair@users.noreply.github.com>
2023-12-01 11:33:58 -0700

    Create 7_Making Plots.R

---
+++ Code/7_Making Plots.R
@@ -0,0 +1,15 @@
+#7 Making Charts - Take Two
+
+#Library needed
+library(tidyverse)
+library(readxl)
+library(haven)
+library(labelled)
+library(dplyr)
+library(tidyr)
+library(writexl)
+library(ggplot2)
+library(RColorBrewer)
+
+#Read in the data set that was created in Step 6
+
